9a40938aeb8c749c121a2c7522d75b00be0a43b7
Sérgio Santos <srui.santos@gmail.com>
2020-03-05 22:02:05 +0000

    Updates

---
+++ precache-manifest.d49d550a7369822d92ad731061cc3f21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d235e5b4f95fda2e93e06465b31943c0",
+    "url": "/ubicacion/index.html"
+  },
+  {
+    "revision": "a375949f17ec570fdd5a",
+    "url": "/ubicacion/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "af7a8753f4db413240f2",
+    "url": "/ubicacion/static/js/2.5869f1f8.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/ubicacion/static/js/2.5869f1f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a375949f17ec570fdd5a",
+    "url": "/ubicacion/static/js/main.72715108.chunk.js"
+  },
+  {
+    "revision": "138ec61bd2778fc70773",
+    "url": "/ubicacion/static/js/runtime-main.e06a4f7c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ubicacion/static/media/logo.5d5d9eef.svg"
+  }
+]);
