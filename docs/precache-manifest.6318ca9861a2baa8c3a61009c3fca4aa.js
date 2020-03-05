7b9016ab4086abc9bcc2ede667e68ce799e255c4
Sérgio Santos <srui.santos@gmail.com>
2020-03-05 21:03:47 +0000

    build

---
+++ docs/precache-manifest.6318ca9861a2baa8c3a61009c3fca4aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e678bf65d9e45741b6ee03585fb7554",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75986a0442d0e34a719f",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c93abc6b7da333cf4265",
+    "url": "/static/js/2.d7fd4e07.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.d7fd4e07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75986a0442d0e34a719f",
+    "url": "/static/js/main.9c634014.chunk.js"
+  },
+  {
+    "revision": "c9b997d836b9be35823a",
+    "url": "/static/js/runtime-main.1e2c1456.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
